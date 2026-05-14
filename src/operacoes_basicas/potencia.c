358f41d0bb42a5246d6aed30a3ae07cd37d9f921
Eduardotg3250 <dudu29042008@gmail.com>
2026-05-14 20:46:33 -0300

    Update potencia.c
    Este código tem a função de calcular a potência a partir de dois valores que o usuário inserir.

--- src/operacoes_basicas/potencia.c
+++ src/operacoes_basicas/potencia.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
-#include "potencia.h"
+#include <math.h>
+// Este código tem a finalidade de calcular o resultado de uma exponenciação a partir dois valores
 
-void potencia() {
-    printf("\n");
-    printf("========================================\n");
-    printf("  Essa funcionalidade ainda não está\n");
-    printf("  implementada :(\n");
-    printf("========================================\n");
-
+// função para calcular a potência
+int potencia(int base, int expoente) { 
+    int resultado;
+    resultado = (int) pow(base, expoente);
+    return resultado;
 }
+
+// executar o código
+int main() {
+    int base, expoente, resultado;
+
+    // recebe o valor base do usuário
+    printf("Digite o numero base:\n");
+    scanf("%d", &base);
+
+    // recebe o expoente do usuário
+    printf("Digite o numero expoente:\n");
+    scanf("%d", &expoente);
+
+    // o resultado é calculado ao chamar a função potencia
+    resultado = potencia(base, expoente);
+
+    // Revela o resultado ao usuário
+    printf("O resultado de %d elevado a %d é: %d\n", base, expoente, resultado);
+}
+
